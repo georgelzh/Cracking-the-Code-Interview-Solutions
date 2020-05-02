b5262e328d9f1216a43b304dedceec687d7bdbeb
georgelzh <zhihongli@bennington.edu>
2020-05-02 13:03:23 -0400

    update comment

--- arrayList-n-string/palindrome-permutation-1.py
+++ arrayList-n-string/palindrome-permutation-1.py
@@ -4,6 +4,21 @@ a permutation of a palin­ drome. A palindrome is a word or phrase that is the
 same forwards and backwards. A permutation is a rearrangement of letters. 
 The palindrome does not need to be limited to just dictionary words.
 
+brute force,  NOTICE: if this code is written because I thought the program
+is required to also output the palindrome permutation set. Therefore, I 
+generated all possible solution and filtered them all here. I might be hard to
+check string longer than 15 chars. However, this program includes the way to 
+check whether a string is a palindrome or not. 
+
+simply check number of chars make sure all number of chars (such as 'a'..) is divisible by 2,
+except for one.
+Case 1: all char appears N times, and N%2 == 0
+Case 2: all char appears N times, and N%2 ==0, except for one char that is allowed to appears odd times. 
+
+Input: Tact Coa
+Output: True (permutations: "taco cat", "atco eta", etc.) Hints:#106,#121,#134,#136
+
+
 """
 
 def is_palindrome_permutation(str):
