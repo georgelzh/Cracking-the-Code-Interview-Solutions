822a8c74420afa7119eb54443d59a0c6eb45d5ee
georgelzh <zhihongli@bennington.edu>
2020-05-03 11:06:36 -0400

    rorate matrix

---
+++ arrayList-n-string/rotateMatrix.py
@@ -0,0 +1,51 @@
+"""
+Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the 
+image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+Hints:#51, #100
+
+under this condition, the algorithm fails to check whether its a n*n matrix
+arr = [
+		["1"],
+		['3','4']
+		]
+
+"""
+
+
+
+arr = [
+		["1",'2','3'],
+		['4','5','6'],
+		['7','8','9']
+		]
+
+def rorateMetrix(matrix):
+	if len(matrix) == 0 or len(matrix)!= len(matrix[0]):
+		return False
+	n = len(matrix)
+	for layer in range(int(n/2)):
+		first = layer
+		last = n - 1 - layer
+		for i in range(first, last):
+			offset = i - first
+			top = matrix[first][i] # SAVE top
+			# top = left
+			matrix[first][i] = matrix[last - offset][first]
+
+			# left = bottom
+			matrix[last - offset][first] = matrix[last][last - offset]
+
+			# bottom = right
+			matrix[last][last - offset] = matrix[i][last - offset]
+
+			# right = top
+			matrix[i][last - offset] = top
+
+			# for understanding
+			# print("top: ", first, " ", [i])
+			# print("left: ", last - offset, " ", [first])
+			# print("bottom: ", last, " ", last - offset)
+			# print("right: ", i, " ", last - offset)
+	return arr
+
+assert rorateMetrix(arr) == [['7', '4', '1'], ['8', '2', '6'], ['9', '5', '3']], "code is not doing the job"
