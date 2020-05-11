29335999a3972e732bfcb65dff55a1a3d7d7bb38
georgelzh <33303558+georgelzh@users.noreply.github.com>
2020-05-11 13:28:28 -0400

    yeahhh!

--- treeAndGraphs/listOfDepths.py
+++ treeAndGraphs/listOfDepths.py
@@ -3,6 +3,26 @@
 a linked list of all the nodes at each depth (e.g., if you have a tree
 with depth D, you'll have D linked lists). Hints: #107, #123, #135
 
+the concept is to use an arr that's outside of the recursive function to store
+the change of list. Since we do not know the depth of the array, we will just
+start with empty list. whenerver we reach a new level, we create a new list in
+the original list. so we have [[]* number of depths]
+
+1. we use pre Order Traversal to approach this problem because this way,
+we keep track of the depth we are at.
+
+2. we need a function that pass down the list, and number of depths to append
+values at the right list. eg: if we have a value 5 at depth 8 (start counting
+from 0). then we will have list[5].append(5) so that we have this value.
+
+process :
+start from root, we append the root value list[0].append(root.value),
+then we check the if the root have left if it does, we pass depth number as 0+1,
+and pass down the list that has root value in it. this time list[0+1].append(node.value)
+will happen. then we move to right node and do the same thing. recursively,
+all the node will be added at the right level into the list.
+
+in the end we return the arr.
 """
 class Node:
     def __init__(self,value=None):
