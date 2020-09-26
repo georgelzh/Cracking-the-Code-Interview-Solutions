cfb42f1faabebc4ab27684f6d82dc2d9b367627a
Zhihong Li <zhihongli@bennington.edu>
2020-09-26 17:31:24 -0400

    recursion triple step problem

---
+++ recursionAndDynamicProgramming/tripleStep.py
@@ -0,0 +1,20 @@
+"""
+Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
+steps at a time. Implement a method to count how many possible ways the child can run up the
+stairs
+Hints: #152, #178, #217, #237, #262, #359
+"""
+
+def numberOfWaysRunStairs(n):
+    cache = {0: 1, 1: 1, 2: 2, 3: 3}
+    if n < 0:
+        return 0
+    return helper(n, cache)
+
+def helper(n, cache):
+    if n in cache:
+        return cache[n]
+    cache[n] = helper(n - 1, cache) + helper(n - 2, cache) + helper(n - 3, cache)
+    return cache[n]
+
+print(numberOfWaysRunStairs(-2))
