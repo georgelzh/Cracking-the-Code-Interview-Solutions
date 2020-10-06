e1c509fe5124a8f2ccbbfb8b1adf8be42a928d01
Zhihong Li <zhihongli@bennington.edu>
2020-10-06 17:37:33 -0400

    hard one

--- recursionAndDynamicProgramming/towersOfHanoi.py
+++ recursionAndDynamicProgramming/towersOfHanoi.py
@@ -7,7 +7,43 @@ constraints:
 (2) A disk is slid off the top of one tower onto another tower.
 (3) A disk cannot be placed on top of a smaller disk.
 Write a program to move the disks from the first tower to the last using stacks.
-Hints: #744, #224, #250, #272, #318
-
+Hints: #144, #224, #250, #272, #318
 
 """
+class Tower:
+    def __init__(self, index):
+        self.index = index
+        self.disks = []
+
+    def add(self, d):
+        print(d)
+        if self.disks and self.disks[len(self.disks) - 1] <= d:
+            print("error")
+        else:
+            self.disks.append(d)
+
+    def moveTopTo(self, dest):
+        top = self.disks.pop()
+        dest.add(top)
+
+    def moveDisks(self, n, dest, buffer):
+        if n > 0:
+            self.moveDisks(n - 1, buffer, dest)
+            self.moveTopTo(dest)
+            buffer.moveDisks(n - 1, dest, self)
+            # move n - 1 from buffer to dest. destination to buffer. 
+            # bc line 31 here we are using dest as buffer, and we
+
+def towersOfHanoi(n):
+    towers = []
+    for i in range(n):
+        towers.append(Tower(i))
+    
+    for i in range(n, 0, -1):
+        towers[0].add(i)
+    
+    towers[0].moveDisks(n, towers[2], towers[1])
+    return towers[2]
+    
+a = towersOfHanoi(10)
+print(a.disks)
